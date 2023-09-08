1072136d67eabd7fd09e08c069789c44d907ca36
NguyenCongAnh1 <nguyenanh342000@gmail.com>
2023-09-08 14:11:57 +0700

    commit 5

--- src/tests/handlers/endpointSpec.ts
+++ src/tests/handlers/endpointSpec.ts
@@ -4,8 +4,6 @@ import app from '../../server'
 import express from 'express';
 import userRoutes from '../../handlers/users';
 
-
-
 const request = supertest(app);
 
 fdescribe("User Endpoints", () => {
@@ -29,7 +27,7 @@ fdescribe("User Endpoints", () => {
 
     it('Test /users GET route ', async () => {
 
-        await request.post('/users')
+        await request.get('/users')
             .auth('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJmaXJzdF9uYW1lIjoiQWxpY2UiLCJsYXN0X25hbWUiOiJKb2huc29uIiwicGFzc3dvcmRfZGlnZXN0IjoiJDJiJDEwJHhTWDR3Z1lXWHhDYVRhS0JYUXhZMS5EOGprV20ySVBmUk5sbWRoRWNsa0xBQWF1Y0ZhcE5DIn0sImlhdCI6MTY5MzkyNTQ3OH0.uske3kXfhh3RgUCXn7gS8glewJo9Ar3mJmOFVBFGamA', { type: 'bearer' })
             .send()
             .expect(200)
